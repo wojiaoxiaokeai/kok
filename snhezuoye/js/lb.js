aa3866549dbf95777a20dff79b75b0e8e14cf662
wojiaoxiaokeai <38481917+wojiaoxiaokeai@users.noreply.github.com>
2018-05-29 09:56:58 +0800

    Add files via upload

---
+++ snhezuoye/js/lb.js
@@ -0,0 +1,23 @@
+/* 
+* @Author: admin
+* @Date:   2018-05-25 09:57:21
+* @Last Modified by:   admin
+* @Last Modified time: 2018-05-25 10:35:38
+*/
+
+'use strict';
+var speed=10; //数字越大速度越慢
+var tab=document.getElementById("demo");
+var tab1=document.getElementById("demo1");
+var tab2=document.getElementById("demo2");
+tab2.innerHTML=tab1.innerHTML;
+function Marquee(){
+if(tab2.offsetWidth-tab.scrollLeft<=0)
+tab.scrollLeft-=tab1.offsetWidth
+else{
+tab.scrollLeft++;
+}
+}
+var MyMar=setInterval(Marquee,speed);
+tab.onmouseover=function() {clearInterval(MyMar)};
+tab.onmouseout=function() {MyMar=setInterval(Marquee,speed)};
